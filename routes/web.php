779f230cd5c273f29a86340eb086d045d50b84c2
Divinewill <51236443+divinoeze@users.noreply.github.com>
2020-12-31 13:17:17 +0100

    Application Setup

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('blog.index');
+});
+
+Route::get('/blog/post/show', function () {
+    return view('blog.show');
+})->name('show.blog.post');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
