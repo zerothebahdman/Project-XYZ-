784dd1a2902239608ae235f36ed2f85914742ca4
Divinewill <51236443+divinoeze@users.noreply.github.com>
2021-01-23 23:29:04 +0100

    Setup backend dashoard (Update). Disabled registeration route

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -12,7 +12,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('backend/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ Auth::user()->profile_photo_path }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="javascript:;" class="d-block">{{ Auth::user()->name }}</a>
